dd370d77022a50555e63be7215821eecfc5f7149
Michael Keiser <michi.keiser@gmail.com>
2019-10-19 17:56:20 +0200

    Add projects for WPF

---
+++ MikSoft.Docua.Client/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace MikSoft.Docua.Client
+{
+    using System.Windows;
+
+    using Prism.DryIoc;
+    using Prism.Ioc;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override Window CreateShell()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
